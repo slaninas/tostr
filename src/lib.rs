6a5247724faabb9f819bc0c5969cdd24404c24ec
slaninas <slaninas@pm.me>
2022-07-06 17:21:54 +0200

    Reorganization

--- src/lib.rs
+++ src/lib.rs
@@ -1,63 +1,168 @@
 use futures_util::sink::SinkExt;
+use futures_util::StreamExt;
 use secp256k1::{rand, KeyPair, Secp256k1};
 
-// TODO: Add remaining fields
-pub struct Event {
-    id: String,
-    pubkey: String,
-    created_at: u64,
-    kind: u64,
-    content: String,
-    sig: String,
+use log::{debug, info};
+
+pub mod nostr;
+
+pub struct Config {
+    pub secret: String,
+    pub refresh_interval_secs: u64,
+    pub relays: Vec<String>,
+    pub follow: Vec<String>,
 }
 
-impl Event {
-    pub fn new(secret: String, content: String, created_at: u64) -> Self {
-        let secp = Secp256k1::new();
+pub fn parse_config(path: &std::path::Path) -> Config {
+    let get_value = |line: String| line.split('=').collect::<Vec<_>>()[1].to_string();
 
-        let key_pair = secp256k1::KeyPair::from_seckey_str(&secp, &secret).unwrap();
-        let (pubkey, parity) = key_pair.x_only_public_key();
-        // println!("secret {}", key_pair.display_secret());
-        // println!("pubkey {}", pubkey);
+    let content = std::fs::read_to_string(path).expect("Config reading failed.");
 
-        let msg = format!(r#"[0,"{}",{},1,[],"{}"]"#, pubkey, created_at, content);
-        let id =
-            secp256k1::Message::from_hashed_data::<secp256k1::hashes::sha256::Hash>(msg.as_bytes());
+    let mut secret = String::new();
+    let mut refresh_interval_secs = 0;
+    let mut relays = vec![];
+    let mut follow = vec![];
 
-        let signature = secp.sign_schnorr(&id, &key_pair);
+    for line in content.lines() {
+        let line = line.to_string();
 
-        Event {
-            id: id.to_string(),
-            pubkey: pubkey.to_string(),
-            created_at,
-            kind: 1,
-            content,
-            sig: signature.to_string(),
+        if line.starts_with("secret") {
+            secret = get_value(line);
+        } else if line.starts_with("refresh_interval_secs") {
+            refresh_interval_secs = get_value(line)
+                .parse::<u64>()
+                .expect("Failed to parse the refresh interval.");
+        } else if line.starts_with("add_relay") {
+            relays.push(get_value(line))
+        } else if line.starts_with("add_follow") {
+            follow.push(get_value(line))
         }
     }
 
-    pub fn format(&self) -> String {
-        format!(
-            r#"["EVENT",{{"id": "{}", "pubkey": "{}", "created_at": {}, "kind": {}, "tags": [], "content": "{}", "sig": "{}"}}]"#,
-            self.id, self.pubkey, self.created_at, self.kind, self.content, self.sig
-        )
-    }
-    pub fn print(&self) {
-        println!("{}", self.format());
-    }
+    assert!(secret.len() > 0);
+    assert!(refresh_interval_secs > 0);
+    assert!(relays.len() > 0);
+    assert!(follow.len() > 0);
 
-    pub async fn send(&self, address: &String) {
-        // TODO: Keep the connection alive
-        let (mut ws_stream, response) =
-            tokio_tungstenite::connect_async(url::Url::parse(address).unwrap())
-                .await
-                .expect("Can't connect");
-
-        ws_stream
-            .send(tungstenite::Message::Text(self.format().into()))
-            .await
-            .unwrap();
-        ws_stream.close(None).await.unwrap();
+    Config {
+        secret,
+        refresh_interval_secs,
+        relays,
+        follow,
     }
 }
 
+pub async fn update_user(
+    username: String,
+    secret: String,
+    relays: Vec<String>,
+    refresh_interval_secs: u64,
+) {
+    let mut since: chrono::DateTime<chrono::offset::Local> = std::time::SystemTime::now().into();
+    loop {
+        debug!("Going to sleep for {} s", refresh_interval_secs);
+        tokio::time::sleep(std::time::Duration::from_secs(refresh_interval_secs)).await;
+
+        let new_tweets = get_new_tweets(&username, since).await;
+        since = std::time::SystemTime::now().into();
+
+        // twint returns newest tweets first, reverse the Vec here so that tweets are send to relays
+        // in order they were published. Still the created_at field can easily be the same so in the
+        // end it depends on how the relays handle it
+        for tweet in new_tweets.iter().rev() {
+            send_tweet(tweet, &secret, &relays).await;
+        }
+        // break;
+    }
+}
+
+#[derive(Debug)]
+struct Tweet {
+    date: String,
+    username: String,
+    tweet: String,
+    link: String,
+}
+
+impl std::fmt::Debug for Config {
+    fn fmt(&self, fmt: &mut std::fmt::Formatter) -> std::fmt::Result {
+        fmt.debug_struct("Config")
+            .field("secret", &"***")
+            .field("refresh_interval_secs", &self.refresh_interval_secs)
+            .field("relays", &self.relays)
+            .field("follow", &self.follow)
+            .finish()
+    }
+}
+
+async fn send_tweet(tweet: &Tweet, secret: &String, relays: &Vec<String>) {
+    let formatted = format!(
+        "[@{}@twitter.com]({}): {}",
+        tweet.username, tweet.link, tweet.tweet
+    );
+
+    let timestamp = std::time::SystemTime::now()
+        .duration_since(std::time::UNIX_EPOCH)
+        .unwrap()
+        .as_secs();
+    let event = nostr::Event::new(secret.clone(), formatted, timestamp);
+
+    debug!("new event: {}", event.format());
+
+    for relay in relays {
+        debug!("Sending >{}< to {}", event.format(), relay);
+        event.send(relay).await;
+    }
+}
+
+async fn get_new_tweets(
+    username: &String,
+    since: chrono::DateTime<chrono::offset::Local>,
+) -> Vec<Tweet> {
+    debug!("Checking new tweets from {}", username);
+    let workfile = format!("{}_workfile.csv", username);
+
+    let since = "2022-07-03 20:39:17";
+    let cmd = format!(
+        "twint -u {} --since \"{}\" --csv -o {}",
+        username,
+        // since.format(DATE_FORMAT_STR),
+        since,
+        workfile
+    );
+    debug!("Running >{}<", cmd);
+    let mut output = async_process::Command::new("bash")
+        .arg("-c")
+        .arg(cmd)
+        .status()
+        .await
+        .unwrap();
+
+    let mut new_tweets = vec![];
+    match std::fs::read_to_string(workfile.clone()) {
+        Ok(content) => {
+            std::fs::remove_file(workfile).unwrap();
+
+            let mut csv = content.lines().collect::<Vec<_>>();
+
+            let header = csv[0].split("\t").collect::<Vec<_>>();
+
+            for i in 1..csv.len() {
+                let line = csv[i].split("\t").collect::<Vec<_>>();
+                new_tweets.push(Tweet {
+                    date: format!("{} {} {}", line[3], line[4], line[5]),
+                    username: line[7].to_string(),
+                    tweet: line[10].to_string(),
+                    link: line[20].to_string(),
+                });
+            }
+
+            info!("Found {} new tweets from {}", new_tweets.len(), username);
+        }
+        Err(_) => {
+            info!("No new tweets from {} found", username);
+        }
+    }
+
+    new_tweets
+}
--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,7 @@ async fn main() {
         .init();
 
     let config_path = std::path::PathBuf::from("config");
-    let config = parse_config(&config_path);
+    let config = tostr::parse_config(&config_path);
     debug!("{:?}", config);
 
     let time: chrono::DateTime<chrono::offset::Local> = std::time::SystemTime::now().into();
@@ -30,7 +30,7 @@ async fn main() {
         let relays = config.relays.clone();
         debug!("Spawning update user task for {}", username);
         handles.push(tokio::spawn(async move {
-            update_user(username, secret, relays, config.refresh_interval_secs).await;
+            tostr::update_user(username, secret, relays, config.refresh_interval_secs).await;
         }));
     }
 
@@ -38,157 +38,3 @@ async fn main() {
         tokio::join!(handle);
     }
 }
-
-#[derive(Debug)]
-struct Tweet {
-    date: String,
-    username: String,
-    tweet: String,
-    link: String,
-}
-
-struct Config {
-    secret: String,
-    refresh_interval_secs: u64,
-    relays: Vec<String>,
-    follow: Vec<String>,
-}
-
-impl std::fmt::Debug for Config {
-    fn fmt(&self, fmt: &mut std::fmt::Formatter) -> std::fmt::Result {
-        fmt.debug_struct("Config")
-            .field("secret", &"***")
-            .field("refresh_interval_secs", &self.refresh_interval_secs)
-            .field("relays", &self.relays)
-            .field("follow", &self.follow)
-            .finish()
-    }
-}
-
-async fn update_user(
-    username: String,
-    secret: String,
-    relays: Vec<String>,
-    refresh_interval_secs: u64,
-) {
-    let mut since: chrono::DateTime<chrono::offset::Local> = std::time::SystemTime::now().into();
-    loop {
-        debug!("Going to sleep for {} s", refresh_interval_secs);
-        tokio::time::sleep(std::time::Duration::from_secs(refresh_interval_secs)).await;
-
-        let new_tweets = get_new_tweets(&username, since).await;
-        since = std::time::SystemTime::now().into();
-
-        // twint returns newest tweets first, reverse the Vec here so that tweets are send to relays
-        // in order they were published. Still the created_at field can easily be the same so in the
-        // end it depends on how the relays handle it
-        for tweet in new_tweets.iter().rev() {
-            send_tweet(tweet, &secret, &relays).await;
-        }
-        // break;
-    }
-}
-
-async fn send_tweet(tweet: &Tweet, secret: &String, relays: &Vec<String>) {
-    let formatted = format!(
-        "[@{}@twitter.com]({}): {}",
-        tweet.username, tweet.link, tweet.tweet
-    );
-
-    let timestamp = std::time::SystemTime::now().duration_since(std::time::UNIX_EPOCH).unwrap().as_secs();
-    let event = tostr::Event::new(secret.clone(), formatted, timestamp);
-
-    debug!("new event: {}", event.format());
-
-    for relay in relays {
-        debug!("Sending >{}< to {}", event.format(), relay);
-        event.send(relay).await;
-    }
-}
-
-async fn get_new_tweets(username: &String, since: chrono::DateTime<chrono::offset::Local>) -> Vec<Tweet> {
-    debug!("Checking new tweets from {}", username);
-    let workfile = format!("{}_workfile.csv", username);
-
-    let cmd = format!(
-        "twint -u {} --since \"{}\" --csv -o {}",
-        username,
-        since.format(DATE_FORMAT_STR),
-        workfile
-    );
-    debug!("Running >{}<", cmd);
-    let mut output = async_process::Command::new("bash")
-        .arg("-c")
-        .arg(cmd)
-        .status().await
-        .unwrap();
-
-
-    let mut new_tweets = vec![];
-    match std::fs::read_to_string(workfile.clone()) {
-        Ok(content) => {
-            std::fs::remove_file(workfile).unwrap();
-
-            let mut csv = content.lines().collect::<Vec<_>>();
-
-            let header = csv[0].split("\t").collect::<Vec<_>>();
-
-            for i in 1..csv.len() {
-                let line = csv[i].split("\t").collect::<Vec<_>>();
-                new_tweets.push(Tweet {
-                    date: format!("{} {} {}", line[3], line[4], line[5]),
-                    username: line[7].to_string(),
-                    tweet: line[10].to_string(),
-                    link: line[20].to_string(),
-                });
-            }
-
-            info!("Found {} new tweets from {}", new_tweets.len(), username);
-        }
-        Err(_) => {
-            info!("No new tweets from {} found", username);
-        }
-    }
-
-    new_tweets
-}
-
-fn parse_config(path: &std::path::Path) -> Config {
-    let get_value = |line: String| line.split('=').collect::<Vec<_>>()[1].to_string();
-
-    let content = std::fs::read_to_string(path).expect("Config reading failed.");
-
-    let mut secret = String::new();
-    let mut refresh_interval_secs = 0;
-    let mut relays = vec![];
-    let mut follow = vec![];
-
-    for line in content.lines() {
-        let line = line.to_string();
-
-        if line.starts_with("secret") {
-            secret = get_value(line);
-        } else if line.starts_with("refresh_interval_secs") {
-            refresh_interval_secs = get_value(line)
-                .parse::<u64>()
-                .expect("Failed to parse the refresh interval.");
-        } else if line.starts_with("add_relay") {
-            relays.push(get_value(line))
-        } else if line.starts_with("add_follow") {
-            follow.push(get_value(line))
-        }
-    }
-
-    assert!(secret.len() > 0);
-    assert!(refresh_interval_secs > 0);
-    assert!(relays.len() > 0);
-    assert!(follow.len() > 0);
-
-    Config {
-        secret,
-        refresh_interval_secs,
-        relays,
-        follow,
-    }
-}
-
