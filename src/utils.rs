603fe33c079ebd323a6fd1f7c4c2ff88b3e0d1a0
slaninas <slaninas@pm.me>
2022-07-18 21:51:33 +0200

    Follow links redirects

--- src/utils.rs
+++ src/utils.rs
@@ -154,6 +154,9 @@ pub async fn get_new_tweets(
         }
     }
 
+    // Follow links to the final destinations
+    follow_links(&mut new_tweets).await;
+
     new_tweets
 }
 
@@ -187,4 +190,37 @@ pub fn get_random_keypair() -> secp256k1::KeyPair {
     let secp = secp256k1::Secp256k1::new();
     let secret = secp256k1::SecretKey::new(&mut rand::thread_rng());
     secret.keypair(&secp)
+}
+
+async fn follow_links(tweets: &mut Vec<Tweet>) {
+    let finder = linkify::LinkFinder::new();
+
+    for tweet in tweets {
+        let text = &tweet.tweet;
+        let links: Vec<_> = finder.links(&text).collect();
+
+        let mut curr_pos = 0;
+        let mut final_tweet = String::new();
+
+        for link in &links {
+            let start = link.start();
+            let end = link.end();
+
+            let request = reqwest::get(link.as_str()).await;
+
+            let final_url = match request {
+                Ok(response) => response.url().as_str().to_string(),
+                Err(e) => { debug!("Failed to follow link >{}< ({}), using orignal url", link.as_str().to_string(), e); link.as_str().to_string()},
+            };
+
+            final_tweet.push_str(&text[curr_pos..start]);
+            final_tweet.push_str(&final_url);
+            curr_pos = end;
+        }
+
+        final_tweet.push_str(&text[curr_pos..]);
+
+        debug!("follow_links: orig. tweet >{}<, final tweet >{}<", text, final_tweet);
+        tweet.tweet = final_tweet;
+    }
 }
